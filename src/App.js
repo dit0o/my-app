df8a020503cb8b8d6352be9b54b2761d3d3f13e3
dit0o <gaganam6985@gmail.com>
2024-12-24 17:37:42 +0530

    Add simple router implementation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import Review from './Review';
-function App() {
-  return (
-    <main>
-      <section className='container'>
-        <div className='title'>
-          <h2>our reviews</h2>
-          <div className='underline'></div>
-        </div>
-        <Review />
-      </section>
-    </main>
-  );
-}
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Product from "./Pages/Product";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import {Routes,Route}from "react-router-dom";
 
-export default App;
+const App=(()=>{
+  return(
+    <>
+    <Navbar/>
+    <div className="container">
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/products" element={<Product/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      
+    </Routes>
+    </div>
+    
+    </>
+  )
+
+
+})
+export default App
