9285ee1e21f9a3f8485ae53bd82ec05d1878aeac
dit0o <gaganam6985@gmail.com>
2024-12-24 19:29:56 +0530

    usenavigate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { createElement } from "react";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Product from "./Pages/Product";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
-import {Routes,Route}from "react-router-dom";
+import {Routes,Route, createBrowserRouter, createRoutesFromElements, RouterProvider}from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
 
 const App=(()=>{
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout/>}>
+        <Route index element={<Home/>}/>
+      <Route path="products" element={<Product/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+
+      </Route>
+    )
+  )
   return(
     <>
-    <Navbar/>
-    <div className="container">
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/products" element={<Product/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      
-    </Routes>
-    </div>
+    
+    <RouterProvider router={router}/>
     
     </>
   )
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
+import { NavLink,replace,useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const navigate=useNavigate()
     return (
         <div className='navbar'>
              
             <ul>
-               <Link to={"/"}><li>Home</li></Link> 
-               <Link to={"/about"}><li>About</li></Link> 
-               <Link to={"/products"}><li>Product</li></Link> 
-               <Link to={"/contact"}><li>Contact</li></Link> 
+               <NavLink to={"/"}><li>Home</li></NavLink> 
+               <NavLink to={"/about"}><li>About</li></NavLink> 
+               <NavLink to={"/products"}><li>Product</li></NavLink> 
+               <NavLink to={"/contact"}><li>Contact</li></NavLink> 
 
             </ul>
-            <button>Get Started</button>
+            <button onClick={()=>navigate('/about',{replace:true})}>Get Started</button>
         </div>
     )
 }
